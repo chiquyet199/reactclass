581cf5c1be08f1a5102d3b19c092620118417606
Quyết Nguyễn <quyetnguyen@MacBook-Pro-cua-Quyet.local>
2019-11-25 19:08:31 +0700

    Fix build

---
+++ class14/src/components/NavBar/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = props => {
+  return <></>;
+};
+
+export default NavBar;
---
+++ class14/src/components/Product/Product.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Product.css";
+
+const Product = props => {
+  return <></>;
+};
+
+export default Product;
