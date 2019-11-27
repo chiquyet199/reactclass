0a6f3c0b53671453eee3d3f81bf74e31fac2e1f4
Quyết Nguyễn <quyetnguyen@MacBook-Pro-cua-Quyet.local>
2019-11-27 19:38:11 +0700

    Init todoapp with hard code UI

--- class15/src/App.js
+++ class15/src/App.js
@@ -1,13 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+function generateUniqueId() {
+  return Math.floor(new Date().getTime() * Math.random()) + "";
+}
+
+function TodoTitle() {
+  return <h1>Todo List</h1>;
+}
+
+function AddForm() {
+  return (
+    <form className="add-todo">
+      <input className="todoitem-input" type="text" />
+      <button className="add-todoitem-button">Add</button>
+    </form>
+  );
+}
+
+function TodoList(params) {
+  return (
+    <ul className="todoitems">
+      <li className="todoitem">nau an</li>
+      <li className="todoitem done">rua chen</li>
+      <li className="todoitem">giat do</li>
+    </ul>
+  );
+}
+
+function FilterButtons() {
   return (
     <div>
-      <h1>Class 15</h1>
+      <button className="show-all filter-button active">All</button>
+      <button className="show-done filter-button  ">Done</button>
+      <button className="show-not-done filter-button  ">Not Done</button>
     </div>
   );
+}
+
+class App extends React.Component {
+  state = {
+    filter: "all",
+    todos: [
+      {
+        id: generateUniqueId(),
+        name: "nau an",
+        done: false
+      },
+      {
+        id: generateUniqueId(),
+        name: "giat do",
+        done: true
+      },
+      {
+        id: generateUniqueId(),
+        name: "trong con ",
+        done: false
+      }
+    ]
+  };
+  render() {
+    return (
+      <div>
+        <TodoTitle />
+        <AddForm />
+        <TodoList />
+        <FilterButtons />
+      </div>
+    );
+  }
 }
 
 export default App;
